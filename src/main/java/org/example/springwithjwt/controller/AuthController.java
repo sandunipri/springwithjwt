0f15d4fb23614ebca50923fb0a467b4adae0052a
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2025-03-05 08:49:07 +0530

    security

--- src/main/java/org/example/springwithjwt/controller/AuthController.java
+++ src/main/java/org/example/springwithjwt/controller/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/auth")
+@CrossOrigin
 public class AuthController {
 
     private final JwtUtil jwtUtil;
--- src/main/java/org/example/springwithjwt/controller/UserController.java
+++ src/main/java/org/example/springwithjwt/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/user")
+@CrossOrigin
 public class UserController {
     private final UserService userService;
     private final JwtUtil jwtUtil;
